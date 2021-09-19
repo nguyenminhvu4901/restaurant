fec5f014c6791689a637485a4c16a54fb47a122a
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-09-19 21:54:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang làm bánh</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class = 'header'>
+        <img class ='logo' src ='img/logo.png' alt =''></img>
+        <a href ='#'>Home</a>
+        <a href ='#'>About</a>
+        <a href ='#'>Foods</a>
+        <a href ='#'>Contact</a>
+    </div>  
+    
+    <div class ='search'>
+        <input name='search'  placeholder="Search for Food..."></input>
+        <button style ='background-color: red;color:white '>Search</button>
+    </div>
+
+    <div class ='image'>
+        <h2> Explore Foods</h2>
+        <img src ='img/pizza.jpg' alt='' style ='height:300px; margin:6px 6px'></img>
+        <img src ='img/burger.jpg' alt='' style ='height:300px; margin:6px 6px'></img>
+        <img src ='img/momo.jpg' alt='' style ='height:300px;margin:6px 6px'></img>
+    </div>
+
+    <div class='menu'>
+        <h2 style='text-align: center'>Foods Menu</h2>
+        <div class ='emyeuthay'>
+        <ul>
+            <li class ='cl01'>
+                <img style =' height:70px'src ='img/menu-pizza.jpg' alt=''></img>
+                <h5>Food Title</h5>
+                <span>$2.3</span>
+                <h6 >Made with Italian Sauce, Chicken, and organice vegetables</h6>
+                <button style ='background:red; color:white'>Order Now</button>
+            </li>
+        </ul>
+
+        <ul>
+            <li class ='cl01'>
+                <img style =' height:70px'src ='img/menu-burger.jpg' alt=''></img>
+                <h5>Smoky Burger</h5>
+                <span>$2.3</span>
+                <h6 >Made with Italian Sauce, Chicken, and organice vegetables</h6>
+                <button style ='background:red; color:white'>Order Now</button>
+            </li>
+        </ul>
+
+        <ul>
+            <li class ='cl01'>
+                <img style =' height:70px'src ='img/menu-burger.jpg' alt=''></img>
+                <h5>Nice Burger</h5>
+                <span>$2.3</span>
+                <h6 >Made with Italian Sauce, Chicken, and organice vegetables</h6>
+                <button style ='background:red; color:white'>Order Now</button>
+            </li>
+        </ul>
+
+        <ul>
+            <li class ='cl01'>
+                <img style =' height:70px'src ='img/menu-pizza.jpg' alt=''></img>
+                <h5>Food Title</h5>
+                <span>$2.3</span>
+                <h6 >Made with Italian Sauce, Chicken, and organice vegetables</h6>
+                <button style ='background:red; color:white'>Order Now</button>
+            </li>
+        </ul>
+
+        <ul>
+            <li class ='cl01'>
+                <img style =' height:70px'src ='img/menu-pizza.jpg' alt=''></img>
+                <h5>Food Title</h5>
+                <span>$2.3</span>
+                <h6 >Made with Italian Sauce, Chicken, and organice vegetables</h6>
+                <button style ='background:red; color:white'>Order Now</button>
+            </li>
+        </ul>
+
+        <ul>
+            <li class ='cl01'>
+                <img style =' height:70px'src ='img/menu-momo.jpg' alt=''></img>
+                <h5>Chicken Steam Momo</h5>
+                <span>$2.3</span>
+                <h6 >Made with Italian Sauce, Chicken, and organice vegetables</h6>
+                <button style ='background:red; color:white'>Order Now</button>
+            </li>
+        </ul>
+    </div>
+        <h4 style ='text-align:center; color:red'>See All Food</h4>
+    </div>
+   
+    <div class='log'>
+        <img style ='height: 40px' alt ='' src ='https://scontent.xx.fbcdn.net/v/t1.15752-9/cp0/241562310_2005700076255065_7948077867378444342_n.png?_nc_cat=106&ccb=1-5&_nc_sid=aee45a&_nc_ohc=hQr4MfNjthoAX-pLPcf&_nc_ht=scontent.xx&oh=5a100f5f0c3ebe5b55550a29f8f1e40c&oe=616CB5CF'></img>
+        <img  style ='height: 40px' alt ='' src = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/cp0/241474871_239067664821646_4658897847860453896_n.png?_nc_cat=107&ccb=1-5&_nc_sid=aee45a&_nc_ohc=Neu0Qo-7Q4sAX-d7vqj&_nc_ht=scontent.xx&oh=4e31b5b4faabcfef84849c75ec0fc9d9&oe=616D4E2B'></img>
+        <img style ='height: 40px' alt ='' src ='https://scontent.xx.fbcdn.net/v/t1.15752-9/cp0/241397862_1459806631065639_2452520555971802737_n.png?_nc_cat=100&ccb=1-5&_nc_sid=aee45a&_nc_ohc=7T5NKyIzOZQAX_ouzvk&_nc_ht=scontent.xx&oh=61ae39adef785708676161b4e97f7a87&oe=616DFA53'></img>
+    </div>
+
+    <div class ='footer'>
+        <h5>All right reserved. Designed By <a style ='color:#f56b81'href="#">Nguyễn Minh Vũ</a></h5>
+    </div>
+</body>
+</html>
